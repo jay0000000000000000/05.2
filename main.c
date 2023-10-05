ee9687f9301cddcd5086c524e89f2e59aacc1c89
jay0000000000000000 <johay03@naver.com>
2023-10-05 17:29:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-   char c; 
-   int num = 0;
+   int sum = 0;
+   int num;
+   int i;
    
-   printf("input a string: ");
-  while ( (c = getchar() ) !='\n')
-  {
-        if (c >= '0'&& c <= '9') //
-           num = num +1; // num++
-  } 
-  
-  printf("no. of digits is %i\n", num); 
+   printf("input a number: ");
+   scanf("%d", &num);
+   
+   for (i=0; i<=num ; i++)
+   {
+       sum = sum + i;
+   }
+   
+  printf("result is %i\n", sum); 
        
   system("PAUSE");	
   return 0;
